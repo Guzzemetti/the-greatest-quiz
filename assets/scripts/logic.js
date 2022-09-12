d46391f51b9edc550afacbafe291f23250cec3c6
John Guzzetta <johnguzzettaiv@gmail.com>
2022-09-12 16:18:40 -0400

    Added questions

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,6 +1,10 @@
 const startPage = document.getElementById("startPage");
 const quizPage = document.getElementById("quizPage");
 
+// const timer = 120;
+const currentQuestionIndex = 0;
+
+
 function change(){
     startPage.setAttribute("class", "hide")
     quizPage.removeAttribute("class")
---
+++ assets/scripts/questions.js
@@ -0,0 +1,58 @@
+const questions = [
+    {
+        title: "What is Amanda's Last Name?",
+        choices: ["Buttron", "Buitrom", "Buitron", "Button"],
+        answer: "Buitron"
+    },
+    {
+        title: "When is Amanda's Birthday?",
+        choices: ["May 19th", "September 17th", "April 8th", "June 15th"],
+        answer: "May 19th"
+    },
+    {
+        title: "What are the names for Amanda's Cats?",
+        choices: ["Gratsby & Winston", "Prince & Mr. Noodle", "Gatsby & Winston", "Winston & Harry"],
+        answer: "Gatsby & Winston"
+    },
+    {
+        title: "When did Amanda and Gage start dating?",
+        choices: ["1/19/2020", "1/19/2018", "Janurary 19th, 2019", "January 19th, 2018"],
+        answer: "Janurary 19th, 2019"
+    },
+    {
+        title: "What is Amanda's Favorite color?",
+        choices: ["Pink", "Green", "Yellow", "Blue"],
+        answer: "Yellow"
+    },
+    {
+        title: "What is Gage's 'Desert Island' Food?",
+        choices: ["Country Fried Steak", "Sheppard's Pie", "Chocolate Chip Cookies", "Pizzaaaaa"],
+        answer: "Sheppard's Pie"
+    },
+    {
+        title: "What is Gage's favorite movie genre?",
+        choices: ["Post-Apocolyptic", "Rom-Com", "Thriller", "Zombie"],
+        answer: "Zombie"
+    },
+    {
+        title: "Where did Gage and Amanda meet?",
+        choices: ["Tabatha's House", "A Party at Stephanies", "In line for the midnight release of My Little Pony 2", "On a boat"],
+        answer: "Tabatha's House"
+    },
+    {
+        title: "Should Amanda and Gage get another cat?",
+        choices: ["Yes", "Definitely", "No", "Almost positively"],
+        answer: "Definitely"
+    },
+    {
+        title: "Will Amanda break up with me after this quiz?",
+        choices: ["I hope not", "No", "Probably", "She should"],
+        answer: "No"
+    },
+]
+
+// {
+//     title: "",
+//     choices: ["", "", "", ""],
+//     answer: ""
+// },
